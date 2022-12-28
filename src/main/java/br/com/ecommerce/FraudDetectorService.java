443a14cbbbadace2b9b45c45ff0939a0f3e32bd2
Bruno Machado <brunomdr46@gmail.com>
2022-12-28 16:16:42 -0300

    Lidando com customizações

--- src/main/java/br/com/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/ecommerce/FraudDetectorService.java
@@ -2,12 +2,15 @@ package br.com.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.Map;
+
 public class FraudDetectorService {
 
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
         try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(),
-                "ECOMMERCE_NEW_ORDER", fraudService::parse, Order.class)) {
+                "ECOMMERCE_NEW_ORDER", fraudService::parse, Order.class,
+                Map.of())) {
             service.run();
         }
     }
