1aa25da0cff0df2c379e27b5f6c412818375b7a2
Bruno Machado <brunomdr46@gmail.com>
2022-12-28 11:38:08 -0300

    Paralelizando e a importância das keys

--- src/main/java/NewOrderMain.java
+++ src/main/java/NewOrderMain.java
@@ -5,14 +5,16 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "1, 1, 1000";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        var key = UUID.randomUUID().toString();
+        var value =  key + ", 1, 1000";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
         Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
@@ -22,7 +24,7 @@ public class NewOrderMain {
                     data.offset() + "/ timestamp " + data.timestamp());
         };
         var email = "Thank you for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
         producer.send(record, callback).get();
         producer.send(emailRecord, callback).get();
     }
