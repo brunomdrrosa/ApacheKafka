57465aff649fa8048d976accb0256fa5b6452909
Bruno Machado <brunomdr46@gmail.com>
2022-12-29 08:54:35 -0300

    Produtores consumidores e o eager de patterns

--- service-fraud-detector/src/main/java/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/FraudDetectorService.java
@@ -3,7 +3,9 @@ package br.com.ecommerce;
 import br.com.bruno.Order;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.math.BigDecimal;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class FraudDetectorService {
 
@@ -16,7 +18,9 @@ public class FraudDetectorService {
         }
     }
 
-    private void parse(ConsumerRecord<String, Order> record) {
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
         System.out.println("------------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
@@ -28,7 +32,18 @@ public class FraudDetectorService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Order processed");
+        var order = record.value();
+        if (isFraud(order)) {
+            System.out.println("Order is a fraud! " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_REJECTED", order.getUserId(), order);
+        } else {
+            System.out.println("Approved: " + order);
+            orderDispatcher.send("ECOMMERCE_ORDER_APPROVED", order.getUserId(), order);
+        }
+    }
+
+    private boolean isFraud(Order order) {
+        return order.getAmount().compareTo(new BigDecimal("4500")) >= 0;
     }
 
 }
